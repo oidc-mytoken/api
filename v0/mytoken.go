f4648ff470f683b0fec65953669b69a1af05f46e
zachmann <gabriel.zachmann@kit.edu>
2021-07-08 17:25:38 +0200

    add db scan value for rotation

--- v0/mytoken.go
+++ v0/mytoken.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"database/sql/driver"
 	"encoding/json"
 	"fmt"
 )
@@ -45,6 +46,24 @@ type Rotation struct {
 	OnAT     bool   `json:"on_AT,omitempty"`
 	OnOther  bool   `json:"on_other,omitempty"`
 	Lifetime uint64 `json:"lifetime,omitempty"`
+}
+
+// Scan implements the sql.Scanner interface.
+func (r *Rotation) Scan(src interface{}) error {
+	if src == nil {
+		return nil
+	}
+	val := src.([]uint8)
+	err := json.Unmarshal(val, &r)
+	return err
+}
+
+// Value implements the driver.Valuer interface
+func (r Rotation) Value() (driver.Value, error) {
+	if !r.OnAT && !r.OnOther {
+		return nil, nil
+	}
+	return json.Marshal(r)
 }
 
 // TokenVersion is a type for the mytoken version
