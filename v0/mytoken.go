2e9bf4a88466dcfd30b9f999e90ec103eb808cb1
zachmann <gabriel.zachmann@kit.edu>
2022-09-02 11:18:42 +0200

    drop subtoken_capabilities

--- v0/mytoken.go
+++ v0/mytoken.go
@@ -8,30 +8,29 @@ import (
 
 // Mytoken is a mytoken Mytoken
 type Mytoken struct {
-	Version              TokenVersion `json:"ver"`
-	TokenType            string       `json:"token_type"`
-	Issuer               string       `json:"iss"`
-	Subject              string       `json:"sub"`
-	ExpiresAt            int64        `json:"exp,omitempty"`
-	NotBefore            int64        `json:"nbf"`
-	IssuedAt             int64        `json:"iat"`
-	AuthTime             int64        `json:"auth_time"`
-	ID                   string       `json:"jti"`
-	SeqNo                uint64       `json:"seq_no"`
-	Name                 string       `json:"name,omitempty"`
-	Audience             string       `json:"aud"`
-	OIDCSubject          string       `json:"oidc_sub"`
-	OIDCIssuer           string       `json:"oidc_iss"`
-	Restrictions         Restrictions `json:"restrictions,omitempty"`
-	Capabilities         Capabilities `json:"capabilities"`
-	SubtokenCapabilities Capabilities `json:"subtoken_capabilities,omitempty"`
-	Rotation             *Rotation    `json:"rotation,omitempty"`
+	Version      TokenVersion `json:"ver"`
+	TokenType    string       `json:"token_type"`
+	Issuer       string       `json:"iss"`
+	Subject      string       `json:"sub"`
+	ExpiresAt    int64        `json:"exp,omitempty"`
+	NotBefore    int64        `json:"nbf"`
+	IssuedAt     int64        `json:"iat"`
+	AuthTime     int64        `json:"auth_time"`
+	ID           string       `json:"jti"`
+	SeqNo        uint64       `json:"seq_no"`
+	Name         string       `json:"name,omitempty"`
+	Audience     string       `json:"aud"`
+	OIDCSubject  string       `json:"oidc_sub"`
+	OIDCIssuer   string       `json:"oidc_iss"`
+	Restrictions Restrictions `json:"restrictions,omitempty"`
+	Capabilities Capabilities `json:"capabilities"`
+	Rotation     *Rotation    `json:"rotation,omitempty"`
 }
 
 // TokenVer is the current Mytoken TokenVersion
 var TokenVer = TokenVersion{
 	Major: 0,
-	Minor: 4,
+	Minor: 5,
 }
 
 // TokenType is constant that can be used for identifying mytokens and to distinguish them from OIDC tokens
--- v0/mytokenRequest.go
+++ v0/mytokenRequest.go
@@ -10,14 +10,13 @@ type MytokenFromMytokenRequest struct {
 // GeneralMytokenRequest is a type that holds all the information that all mytoken requests have in common. It should
 // not be used directly as a request object
 type GeneralMytokenRequest struct {
-	Issuer               string       `json:"oidc_issuer"`
-	GrantType            string       `json:"grant_type"`
-	Restrictions         Restrictions `json:"restrictions"`
-	Capabilities         Capabilities `json:"capabilities"`
-	SubtokenCapabilities Capabilities `json:"subtoken_capabilities"`
-	Name                 string       `json:"name"`
-	ResponseType         string       `json:"response_type"`
-	MaxTokenLen          int          `json:"max_token_len"`
-	Rotation             *Rotation    `json:"rotation"`
-	ApplicationName      string       `json:"application_name"`
+	Issuer          string       `json:"oidc_issuer"`
+	GrantType       string       `json:"grant_type"`
+	Restrictions    Restrictions `json:"restrictions"`
+	Capabilities    Capabilities `json:"capabilities"`
+	Name            string       `json:"name"`
+	ResponseType    string       `json:"response_type"`
+	MaxTokenLen     int          `json:"max_token_len"`
+	Rotation        *Rotation    `json:"rotation"`
+	ApplicationName string       `json:"application_name"`
 }
