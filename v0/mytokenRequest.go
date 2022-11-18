053340a45c584b7895930eb3ee22242fd8121484
zachmann <gabriel.zachmann@kit.edu>
2022-11-18 12:23:18 +0100

    omit empty claims in mytoken requests in json marshalling

--- v0/mytokenRequest.go
+++ v0/mytokenRequest.go
@@ -4,19 +4,19 @@ package api
 type MytokenFromMytokenRequest struct {
 	GeneralMytokenRequest
 	Mytoken                      string `json:"mytoken"`
-	FailOnRestrictionsNotTighter bool   `json:"error_on_restrictions"`
+	FailOnRestrictionsNotTighter bool   `json:"error_on_restrictions,omitempty"`
 }
 
 // GeneralMytokenRequest is a type that holds all the information that all mytoken requests have in common. It should
 // not be used directly as a request object
 type GeneralMytokenRequest struct {
-	Issuer          string       `json:"oidc_issuer"`
-	GrantType       string       `json:"grant_type"`
-	Restrictions    Restrictions `json:"restrictions"`
-	Capabilities    Capabilities `json:"capabilities"`
-	Name            string       `json:"name"`
-	ResponseType    string       `json:"response_type"`
-	MaxTokenLen     int          `json:"max_token_len"`
-	Rotation        *Rotation    `json:"rotation"`
-	ApplicationName string       `json:"application_name"`
+	Issuer          string       `json:"oidc_issuer,omitempty"`
+	GrantType       string       `json:"grant_type,omitempty"`
+	Restrictions    Restrictions `json:"restrictions,omitempty"`
+	Capabilities    Capabilities `json:"capabilities,omitempty"`
+	Name            string       `json:"name,omitempty"`
+	ResponseType    string       `json:"response_type,omitempty"`
+	MaxTokenLen     int          `json:"max_token_len,omitempty"`
+	Rotation        *Rotation    `json:"rotation,omitempty"`
+	ApplicationName string       `json:"application_name,omitempty"`
 }
