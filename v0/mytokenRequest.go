04f7ea8ceacee26665162a412abd22be3a6b7a9a
zachmann <gabriel.zachmann@kit.edu>
2022-12-09 14:27:38 +0100

    add possibility to specify included profiles

--- v0/mytokenRequest.go
+++ v0/mytokenRequest.go
@@ -10,13 +10,14 @@ type MytokenFromMytokenRequest struct {
 // GeneralMytokenRequest is a type that holds all the information that all mytoken requests have in common. It should
 // not be used directly as a request object
 type GeneralMytokenRequest struct {
-	Issuer          string       `json:"oidc_issuer,omitempty"`
-	GrantType       string       `json:"grant_type,omitempty"`
-	Restrictions    Restrictions `json:"restrictions,omitempty"`
-	Capabilities    Capabilities `json:"capabilities,omitempty"`
-	Name            string       `json:"name,omitempty"`
-	ResponseType    string       `json:"response_type,omitempty"`
-	MaxTokenLen     int          `json:"max_token_len,omitempty"`
-	Rotation        *Rotation    `json:"rotation,omitempty"`
-	ApplicationName string       `json:"application_name,omitempty"`
+	Issuer           string           `json:"oidc_issuer,omitempty"`
+	GrantType        string           `json:"grant_type,omitempty"`
+	Restrictions     Restrictions     `json:"restrictions,omitempty"`
+	Capabilities     Capabilities     `json:"capabilities,omitempty"`
+	Name             string           `json:"name,omitempty"`
+	ResponseType     string           `json:"response_type,omitempty"`
+	MaxTokenLen      int              `json:"max_token_len,omitempty"`
+	Rotation         *Rotation        `json:"rotation,omitempty"`
+	ApplicationName  string           `json:"application_name,omitempty"`
+	IncludedProfiles IncludedProfiles `json:"include,omitempty"`
 }
--- v0/profiles.go
+++ v0/profiles.go
@@ -2,10 +2,41 @@ package api
 
 import (
 	"encoding/json"
+
+	"github.com/pkg/errors"
 )
 
 type Profile struct {
 	ID      string          `json:"id"`
 	Name    string          `json:"name"`
 	Payload json.RawMessage `json:"payload"`
+}
+
+type IncludedProfiles []string
+
+// UnmarshalJSON implements the json.Unmarshaler interface
+func (i *IncludedProfiles) UnmarshalJSON(bytes []byte) error {
+	var s string
+	var arr []string
+	if err := json.Unmarshal(bytes, &s); err != nil {
+		err = errors.WithStack(json.Unmarshal(bytes, &arr))
+		*i = arr
+		return err
+	} else {
+		*i = NewIncludedProfiles(s)
+	}
+	return nil
+}
+
+// MarshalJSON implements the json.Marshaler interface
+func (i IncludedProfiles) MarshalJSON() ([]byte, error) {
+	if len(i) == 1 {
+		return json.Marshal(i[0])
+	}
+	return json.Marshal([]string(i))
+}
+
+// NewIncludedProfiles creates a new IncludedProfiles var from the past profile names
+func NewIncludedProfiles(profiles ...string) IncludedProfiles {
+	return profiles
 }
