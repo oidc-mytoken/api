bd4e84c58ad14a5689617796e29ed9f3d10a14ab
zachmann <gabriel.zachmann@kit.edu>
2023-04-12 12:30:14 +0200

    add mom_id to mytoken response if transfer code is returned

--- v0/mytokenResponse.go
+++ v0/mytokenResponse.go
@@ -5,6 +5,7 @@ type MytokenResponse struct {
 	Mytoken      string           `json:"mytoken,omitempty"`
 	MytokenType  string           `json:"mytoken_type"`
 	TransferCode string           `json:"transfer_code,omitempty"`
+	MOMID        string           `json:"mom_id,omitempty"`
 	ExpiresIn    uint64           `json:"expires_in,omitempty"`
 	Restrictions Restrictions     `json:"restrictions,omitempty"`
 	Capabilities Capabilities     `json:"capabilities,omitempty"`
