779e21d98dd949e98c48377f2d306b3a1c5d8c0f
zachmann <gabriel.zachmann@kit.edu>
2023-11-28 10:53:41 +0100

    move TokenVersion to mytoken file; incr TokenVersion

--- v0/mytoken.go
+++ v0/mytoken.go
@@ -1,5 +1,10 @@
 package api
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 // Mytoken is a mytoken Mytoken
 type Mytoken struct {
 	Version      TokenVersion `json:"ver"`
@@ -24,7 +29,7 @@ type Mytoken struct {
 // TokenVer is the current Mytoken TokenVersion
 var TokenVer = TokenVersion{
 	Major: 0,
-	Minor: 6,
+	Minor: 7,
 }
 
 // TokenType is a constant that can be used for identifying mytokens and to distinguish them from OIDC tokens
@@ -37,4 +42,51 @@ const MinShortTokenLen = 32
 type UsedMytoken struct {
 	Mytoken      `json:",inline"`
 	Restrictions []UsedRestriction `json:"restrictions,omitempty"`
+}
+
+// TokenVersion is a type for the mytoken version
+type TokenVersion struct {
+	Major int
+	Minor int
+}
+
+const vFmt = "%d.%d"
+
+// String returns a version string
+func (v TokenVersion) String() string {
+	return fmt.Sprintf(vFmt, v.Major, v.Minor)
+}
+
+// Version returns a version string
+func (v TokenVersion) Version() string {
+	return v.String()
+}
+
+func (v TokenVersion) AtLeast(required TokenVersion) bool {
+	if v.Major > required.Major {
+		return true
+	}
+	if v.Major < required.Major {
+		return false
+	}
+	// major version equal
+	return v.Minor >= required.Minor
+}
+func (v TokenVersion) Before(before TokenVersion) bool {
+	return !v.AtLeast(before)
+}
+
+// MarshalJSON implements the json.Marshaler interface
+func (v TokenVersion) MarshalJSON() ([]byte, error) {
+	return json.Marshal(v.String())
+}
+
+// UnmarshalJSON implements the json.Unmarshaler interface
+func (v *TokenVersion) UnmarshalJSON(data []byte) error {
+	var str string
+	if err := json.Unmarshal(data, &str); err != nil {
+		return err
+	}
+	_, err := fmt.Sscanf(str, vFmt, &v.Major, &v.Minor)
+	return err
 }
