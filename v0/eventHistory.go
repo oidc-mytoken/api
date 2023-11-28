3830af656e666a421afa92e0c63552a149326349
zachmann <gabriel.zachmann@kit.edu>
2023-11-28 10:54:41 +0100

    move events to api

--- v0/eventHistory.go
+++ v0/eventHistory.go
@@ -7,7 +7,7 @@ type EventHistory struct {
 
 // EventEntry is a type holding information about an event
 type EventEntry struct {
-	Event          string `db:"event" json:"event"`
+	Event          Event  `db:"event" json:"event"`
 	Time           int64  `db:"time" json:"time"`
 	Comment        string `db:"comment" json:"comment,omitempty"`
 	ClientMetaData `json:",inline"`
