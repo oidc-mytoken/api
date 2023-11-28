daac221cfdce8624581c7d8ccdf8c32582d28ff8
zachmann <gabriel.zachmann@kit.edu>
2023-11-28 10:53:41 +0100

    move TokenVersion to mytoken file; incr TokenVersion

--- v0/rotation.go
+++ v0/rotation.go
@@ -3,7 +3,6 @@ package api
 import (
 	"database/sql/driver"
 	"encoding/json"
-	"fmt"
 )
 
 // Rotation is a type describing how a mytoken might be rotated
@@ -31,37 +30,4 @@ func (r Rotation) Value() (driver.Value, error) {
 		return nil, nil
 	}
 	return json.Marshal(r)
-}
-
-// TokenVersion is a type for the mytoken version
-type TokenVersion struct {
-	Major int
-	Minor int
-}
-
-const vFmt = "%d.%d"
-
-// String returns a version string
-func (v TokenVersion) String() string {
-	return fmt.Sprintf(vFmt, v.Major, v.Minor)
-}
-
-// Version returns a version string
-func (v TokenVersion) Version() string {
-	return v.String()
-}
-
-// MarshalJSON implements the json.Marshaler interface
-func (v TokenVersion) MarshalJSON() ([]byte, error) {
-	return json.Marshal(v.String())
-}
-
-// UnmarshalJSON implements the json.Unmarshaler interface
-func (v *TokenVersion) UnmarshalJSON(data []byte) error {
-	var str string
-	if err := json.Unmarshal(data, &str); err != nil {
-		return err
-	}
-	_, err := fmt.Sscanf(str, vFmt, &v.Major, &v.Minor)
-	return err
 }
